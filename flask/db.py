270f52f85f5119d37c732bc11b1999ac600f9c63
Adam Meyer <adammeyer@MacBook-Air-2.local>
2019-07-17 14:50:03 +0200

    elo stuff works for 1v1 now. Use eval_match function. Added hostname to db connection

--- flask/db.py
+++ flask/db.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def getData(req):
     # Connect to an existing database
-    conn = psycopg2.connect("dbname=template1 user=hampus")
+    conn = psycopg2.connect("host=10.46.0.147 dbname=template1 user=hampus")
     
     # Open a cursor to perform database operations
     cur = conn.cursor()
@@ -25,6 +25,9 @@ def getData(req):
 def updatePoints(points, nameId, gameId):
     return getData("insert into points (points, id, gameid, gamesplayed) values ("+points+", "+nameId+", "+gameId+", 1) on conflict on constraint points_un do update set points="+points+" where points.id="+nameId+" and points.gameid="+gameId+";")
 
+def updatePlayer(userId, gameId, points):
+    return getData("UPDATE points SET points = "+points+" WHERE id = "+userId+" AND gameid = "+gameId+";")
+
 def addDefPoints(nameId,gameId):
     return getData("insert into points (points, id, gameid, gamesplayed) values (default, "+nameId+", "+gameId+", 0);")
 
@@ -34,8 +37,9 @@ def addPoints(points,nameId,gameId):
 def getPoints(gameId): # Fetching all points for a specific game
     return getData("select points.id, users.name, games.gameid , games.gamename, points.points from users, points, games where users.id = points.id and points.gameid = games.gameid and games.gameid = "+gameId+";")
 
-def getPoints(userId): # Fetch elo points for a specific user
-    return getData("select points from points where id = "+userId+";")
+def getPlyrPoints(userId, gameId): # Fetch elo points for a specific user
+    tuple = getData("select points from points where id = "+userId+" and gameid = "+gameId+";")
+    return tuple[0][0]
 
 def getPlayers():
     return getData("select * from users;")
--- flask/elo.py
+++ flask/elo.py
@@ -1,18 +1,28 @@
+from db import updatePlayer, getPlyrPoints
+
 ELO_K = 70
-res = {
-    "win": 1,
-    "loss": 0
-}
 
 def calc_prob(elo1, elo2):
     return 1.0 / (1.0 + pow(10, (elo1-elo2)/400))
 
-def calc_new(elo1, elo2, is_win):
-    p1 = calc_prob(elo1, elo2)
+def calc_new(w_elo, l_elo):
+    p1 = calc_prob(w_elo, l_elo)
     p2 = 1 - p1
 
-    res1 = 1 if is_win else 0
-    res2 = 1 - res1
+    new_win = w_elo + ELO_K * (1 - p1)
+    new_lsr = l_elo + ELO_K * (0 - p2)
 
-    elo1 = elo1 + ELO_K * (res1 – p1)
-    elo2 = elo2 + ELO_K * (res2 – p2)
+    return (new_win, new_lsr)
+
+def eval_match(winner, loser, game):
+    win_elo = getPlyrPoints(winner, game)
+    print(str(win_elo))
+    lsr_elo = getPlyrPoints(loser, game)
+    print(str(lsr_elo))
+
+
+    new_points = calc_new(win_elo, lsr_elo)
+    print("winner new points: " + str(new_points[0]))
+    print("loser new points: " + str(new_points[1]))
+    updatePlayer(winner, game, str(new_points[0]))
+    updatePlayer(loser, game, str(new_points[1]))
