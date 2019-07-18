632739de2a177e46b79b56856c57980d31b1e1e7
Adam Meyer <adammeyer@MacBook-Air-2.local>
2019-07-18 10:05:48 +0200

    elo propability fix

--- flask/elo.py
+++ flask/elo.py
@@ -6,8 +6,11 @@ def calc_prob(elo1, elo2):
     return 1.0 / (1.0 + pow(10, (elo1-elo2)/400))
 
 def calc_new(w_elo, l_elo):
-    p1 = calc_prob(w_elo, l_elo)
-    p2 = 1 - p1
+    p1 = calc_prob(l_elo, w_elo)
+    p2 = calc_prob(w_elo, l_elo)
+
+    print("Winner " + str(w_elo) + " had " + str(p1) + " chance of winning")
+    print("Loser " + str(l_elo) + " had " + str(p2) + " chance of winning")
 
     new_win = w_elo + ELO_K * (1 - p1)
     new_lsr = l_elo + ELO_K * (0 - p2)
@@ -20,9 +23,9 @@ def eval_match(winner, loser, game):
     lsr_elo = getPlyrPoints(loser, game)
     print(str(lsr_elo))
 
-
     new_points = calc_new(win_elo, lsr_elo)
     print("winner new points: " + str(new_points[0]))
     print("loser new points: " + str(new_points[1]))
+
     updatePlayer(winner, game, str(new_points[0]))
     updatePlayer(loser, game, str(new_points[1]))
